b470a8b616332b0b1bcf07c169146769309b0da1
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-03-04 13:40:02 +0530

    4 March 2023

--- secondMax.cpp
+++ secondMax.cpp
@@ -101,7 +101,7 @@ and prints the second-maximum number among the three.
 void bubbleSort(int arr[],int n=3){
     for(int i = 0; i < n-1;i++){
         for(int j= 0; j < n-i-1;j++){
-            if(arr[j]>arr[j+1]){
+            if(arr[j]>arr[j+1]){ 
                 swap((arr[j]),(arr[j+1]));
             }
         }
